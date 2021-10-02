2c31046539c1d6ed41faa48a1211816dd737b0c3
Semyon Kirekov <semyon@kirekov.com>
2021-10-02 15:49:03 +0300

    Refactor RuleValidatorServiceImplTest

--- src/test/java/com/kirekov/test_levels/service/validation/RuleValidatorServiceImplTest.java
+++ src/test/java/com/kirekov/test_levels/service/validation/RuleValidatorServiceImplTest.java
@@ -6,7 +6,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kirekov.test_levels.entity.RuleTestBuilder;
 import com.kirekov.test_levels.entity.RuleTypeTestBuilder;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayName("RuleValidatorServiceImpl: isRuleValid test cases")
 class RuleValidatorServiceImplTest {
@@ -14,41 +15,20 @@ class RuleValidatorServiceImplTest {
   private final ObjectMapper objectMapper = new ObjectMapper();
   private final RuleValidatorService service = new RuleValidatorServiceImpl(objectMapper);
 
-  @Test
+  @ParameterizedTest
   @DisplayName("Should return true if keys and values match")
-  void shouldReturnTrueIfKeysAndValuesMatch() {
+  @CsvSource({
+      "key,value,key,value",
+      "key,value,key,{\"key\": \"value\"}"
+  })
+  void shouldReturnTrueIfKeysAndValuesMatch(
+      String typeKey, String typeValue,
+      String ruleKey, String ruleValue
+  ) {
     final var ruleType =
-        RuleTypeTestBuilder.builder()
-            .setKey("key")
-            .setValue("value")
-            .build();
+        RuleTypeTestBuilder.builder().setKey(typeKey).setValue(typeValue).build();
     final var rule =
-        RuleTestBuilder.builder()
-            .setKey("key")
-            .setValue("value")
-            .build();
-
-    final var result = service.isRuleValid(rule, ruleType);
-
-    assertTrue(
-        result,
-        "Rule should be valid"
-    );
-  }
-
-  @Test
-  @DisplayName("Should return true if json contains the value")
-  void shouldReturnTrueIfJsonContainsTheValue() {
-    final var ruleType =
-        RuleTypeTestBuilder.builder()
-            .setKey("key")
-            .setValue("value")
-            .build();
-    final var rule =
-        RuleTestBuilder.builder()
-            .setKey("key")
-            .setValue("{\"key\": \"value\"}")
-            .build();
+        RuleTestBuilder.builder().setKey(ruleKey).setValue(ruleValue).build();
 
     final var result = service.isRuleValid(rule, ruleType);
 

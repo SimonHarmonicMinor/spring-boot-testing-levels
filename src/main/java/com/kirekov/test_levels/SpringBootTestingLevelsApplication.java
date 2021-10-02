aef3f0abd4f68b0f74c6e155bcabf5f706dde377
Semyon Kirekov <semyon@kirekov.com>
2021-10-02 11:49:56 +0300

    Init project

---
+++ src/main/java/com/kirekov/test_levels/SpringBootTestingLevelsApplication.java
@@ -0,0 +1,13 @@
+package com.kirekov.test_levels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingLevelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestingLevelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirekov/test_levels/SpringBootTestingLevelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirekov.test_levels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingLevelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

68fc755bbc888400df6f84903b4f818423882d3c
Semyon Kirekov <semyon@kirekov.com>
2021-10-02 12:08:21 +0300

    Add RuleTypeRepository entity

---
+++ src/main/java/com/kirekov/test_levels/repository/RuleTypeRepository.java
@@ -0,0 +1,8 @@
+package com.kirekov.test_levels.repository;
+
+import com.kirekov.test_levels.entity.RuleType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RuleTypeRepository extends JpaRepository<RuleType, Long> {
+
+}

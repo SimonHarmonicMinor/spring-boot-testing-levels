2d938d58712f79cc4b25ad9b4a8b6fc154afcb2d
Semyon Kirekov <semyon@kirekov.com>
2021-10-07 19:27:30 +0300

    Add createRules mocks test

--- src/test/java/com/kirekov/test_levels/service/rule/RuleServiceImplMocksTest.java
+++ src/test/java/com/kirekov/test_levels/service/rule/RuleServiceImplMocksTest.java
@@ -2,6 +2,7 @@ package com.kirekov.test_levels.service.rule;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -12,7 +13,11 @@ import com.kirekov.test_levels.repository.AppRepository;
 import com.kirekov.test_levels.repository.RuleRepository;
 import com.kirekov.test_levels.repository.RuleTypeRepository;
 import com.kirekov.test_levels.service.validation.RuleValidatorService;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -63,5 +68,32 @@ class RuleServiceImplMocksTest {
     assertEquals("name", ruleInfo.getName());
     assertEquals("key", ruleInfo.getKey());
     assertEquals("value", ruleInfo.getValue());
+  }
+
+  @Test
+  @DisplayName("Should create multiple rules successfully")
+  void shouldCreateMultipleRulesSuccessfully() {
+    final var rulesInfo = Stream.of("ip", "host", "user_agent")
+        .map(name -> new RuleInfo(name, "key", "value", 1L, 2L))
+        .collect(Collectors.toList());
+    final var ruleType = RuleTypeTestBuilder.builder().setId(1L).build();
+    when(ruleTypeRepository.findById(1L)).thenReturn(Optional.of(ruleType));
+    final var app = AppTestBuilder.builder().setId(1L).build();
+    when(appRepository.findById(2L)).thenReturn(Optional.of(app));
+    when(ruleValidatorService.isRuleValid(any(), any())).thenReturn(true);
+    final var idGenerator = new AtomicLong();
+    when(ruleRepository.saveAndFlush(any())).thenAnswer(invocation -> {
+      final Rule currRule = invocation.getArgument(0);
+      return currRule.setId(idGenerator.incrementAndGet());
+    });
+
+    final var list = ruleService.createRules(rulesInfo);
+
+    for (Rule rule : list) {
+      assertNotNull(rule.getId());
+      assertTrue(List.of("ip", "host", "user_agent").contains(rule.getName()));
+      assertEquals("key", rule.getKey());
+      assertEquals("value", rule.getValue());
+    }
   }
 }
